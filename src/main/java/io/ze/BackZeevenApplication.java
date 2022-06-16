4b64b8bbace88cea3be16185bd2b4d75fed303e3
Achille MBOUGUENG <achille.mbougueng@yahoo.fr>
2022-06-16 14:51:11 +0200

    Projet initial

---
+++ src/main/java/io/ze/BackZeevenApplication.java
@@ -0,0 +1,13 @@
+package io.ze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackZeevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackZeevenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ze/BackZeevenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackZeevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
